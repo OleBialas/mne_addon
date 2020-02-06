0519e7e79cfd06fbb6685df4a933046ddcc0380a
Ole <bialas.ole@gmx.de>
2020-02-06 12:09:28 +0100

    Change layout using autopep8 and linter flake8

--- mne_addon/analysis.py
+++ mne_addon/analysis.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-_scaling = 10**6 #scaing factor for the data
+_scaling = 10**6  # scaing factor for the data
+
 
 def set_scaling_factor(scaling_factor):
     global _scaling
     _scaling = scaling_factor
+
 
 def noise_rms(epochs):
     """
@@ -15,28 +17,31 @@ def noise_rms(epochs):
     epochs_tmp = epochs.copy()
     n_epochs = epochs._data.shape[0]
     for i in range(n_epochs):
-        if not i%2:
-            epochs_tmp._data[i,:,:] = -epochs_tmp._data[i,:,:]
+        if not i % 2:
+            epochs_tmp._data[i, :, :] = -epochs_tmp._data[i, :, :]
     evoked = epochs_tmp.average().data
-    rms = np.sqrt(np.mean(evoked**2))*_scaling # noise rms in micro volts
+    rms = np.sqrt(np.mean(evoked**2))*_scaling  # noise rms in micro volts
     del epochs_tmp
     return rms
 
-def signal_to_noise(epochs, signal_interval=(0.68,0.72)):
+
+def signal_to_noise(epochs, signal_interval=(0.68, 0.72)):
     """
     Calculate the RMS in the signal interval divide it by the noise rms
     """
     signal = epochs.copy()
-    signal.crop(signal_interval[0],signal_interval[1])
-    noise_rms = noise_rms(epochs)
-    signal_rms = np.sqrt(np.mean(signal.average().data**2))*_scaling
-    snr = signal_rms/noise_rms
+    signal.crop(signal_interval[0], signal_interval[1])
+    n_rms = noise_rms(epochs)
+    s_rms = np.sqrt(np.mean(signal.average().data**2))*_scaling
+    snr = s_rms/n_rms  # signal rms divided by noise rms
     return snr
+
 
 def global_field_power(data):
     """
-    Compute the global field power which is the standard deviation of the squared evoked response.
-    if input data is 3-dimensional it is considered as epoched data and averaged over the first dimension (number
+    Compute the global field power which is the standard deviation of the
+    squared evoked response if input data is 3-dimensional it is considered
+    as epoched data and averaged over the first dimension (number
     of epochs) to obtain the evoked response.
     """
     if data.ndim == 3:
@@ -44,19 +49,23 @@ def global_field_power(data):
     elif data.ndim == 2:
         evoked = data
     else:
-        raise ValueError("Data must be either 2-dimensional (evoked responses) or 3-dimensional (epochs)")
+        raise ValueError("Data must be either 2-dimensional (evoked responses)"
+                         "or 3-dimensional (epochs)")
     return np.std(evoked**2, axis=0)
+
 
 def global_rms(data):
     """
-    Compute the square-root of the mean over all channels after squaring (= root mean square).
-    if input data is 3-dimensional it is considered as epoched data and averaged over the first dimension (number
-    of epochs) to obtain the evoked response.
+    Compute the square-root of the mean over all channels after squaring
+    (= root mean square).If input data is 3-dimensional it is considered as
+    epoched data and averaged over the first dimension (numberof epochs)
+    to obtain the evoked response.
     """
     if data.ndim == 3:
         evoked = np.mean(data, axis=0)
     elif data.ndim == 2:
         evoked = data
     else:
-        raise ValueError("Data must be either 2-dimensional (evoked responses) or 3-dimensional (epochs)")
+        raise ValueError("Data must be either 2-dimensional (evoked responses)"
+                         " or 3-dimensional (epochs)")
     return np.sqrt(np.mean(evoked**2, axis=0))
--- mne_addon/plotting.py
+++ mne_addon/plotting.py
@@ -14,13 +14,13 @@ def plot_multiple_ERP(epochs, n_row=3, n_col=2, outfile=None, title=None, plot_t
     """
     Plot ERP for the different conditions
     """
-    fig, ax = plt.subplots(n_row,n_col, sharex=True, figsize=(16,9))
+    fig, ax = plt.subplots(n_row, n_col, sharex=True, figsize=(16, 9))
     if title:
         fig.suptitle(title)
     conditions = list(epochs.event_id.keys())
-    if n_row*n_col!=len(conditions):
+    if n_row*n_col != len(conditions):
         raise ValueError("Size of the plot and number of experimental"
-         "conditions dont match!")
+                         "conditions dont match!")
     for col in range(n_col):
         for row, condition in zip(range(n_row), conditions[col*n_row:col*n_row+n_row]):
             epochs[condition].average().plot(axes=ax[row, col])
@@ -33,34 +33,38 @@ def plot_multiple_ERP(epochs, n_row=3, n_col=2, outfile=None, title=None, plot_t
         evokeds = [epochs[condition].average() for condition in conditions]
         plot_evoked_topo(evokeds)
 
+
 def plot_fit(x, y, function, p0=None, xtitle=None, ytitle=None):
     """
     Make a scatterplot for the x/y pairs then fit the given function to the
     data and plot the fitted line
     """
-    plt.scatter(x,y, color="k", marker=".", alpha=0.3)
+    plt.scatter(x, y, color="k", marker=".", alpha=0.3)
     par, _ = curve_fit(function, x, y, p0=p0)
-    par = np.round(par,2)
-    label = "a/(x-c)+b with a=%s, b=%s, c=%s" %(par[0],par[1],par[2])
-    plt.plot(np.unique(x),function(np.unique(x),*par), color="k",label=label)
+    par = np.round(par, 2)
+    label = "a/(x-c)+b with a=%s, b=%s, c=%s" % (par[0], par[1], par[2])
+    plt.plot(np.unique(x), function(np.unique(x), *par), color="k", label=label)
     plt.legend()
+
 
 def plot_cluster_analysis(epochs_1, epochs_2, test_stat, clusters, p_values, title):
 
     times = epochs_1.times
     info = epochs_1.info
     for i_c, c in enumerate(clusters):
-        if p_values[i_c] <= 0.05: # found significant cluster
+        if p_values[i_c] <= 0.05:  # found significant cluster
             fig, ax = plt.subplots(2)
             fig.suptitle(title)
-            ax[0].plot(times, np.abs(epochs_1.average().data.mean(axis=0) - epochs_2.average().data.mean(axis=0)))
-            channels = np.unique(np.where(c!=0)[0])
-            samples = np.unique(np.where(c!=0)[1])
+            ax[0].plot(times, np.abs(epochs_1.average().data.mean(
+                axis=0) - epochs_2.average().data.mean(axis=0)))
+            channels = np.unique(np.where(c != 0)[0])
+            samples = np.unique(np.where(c != 0)[1])
             for ch in channels:
                 ax[1].plot(times, test_stat[ch], label=info["ch_names"][ch])
             ax[1].axvspan(times[min(samples)], times[max(samples)], color='r', alpha=0.3)
             plt.legend()
             print(i_c)
+
 
 def compare_evokeds(dataset, groups, mode="gfp", title=None, subtitles=None, tmin=None, tmax=None, vline=None, baseline=None, color_coding=None, ci=None):
     """
@@ -69,34 +73,34 @@ def compare_evokeds(dataset, groups, mode="gfp", title=None, subtitles=None, tmi
     cNorm = colors.Normalize(vmin=min(color_coding.values()), vmax=max(color_coding.values()))
     cmap = cmx.ScalarMappable(norm=cNorm, cmap="cool")
     if mode == "rms":
-        stat_fun=global_rms
+        stat_fun = global_rms
     elif mode == "gfp":
-        stat_fun=global_field_power
+        stat_fun = global_field_power
     else:
         raise ValueError("Mode must be either 'rms' or 'gfp'!")
-    if isinstance(dataset, list) and all(isinstance(d,Evoked) for d in dataset):
+    if isinstance(dataset, list) and all(isinstance(d, Evoked) for d in dataset):
         if ci is not None:
-            raise  ValueError("Need Epochs to compute Confidence Interval")
+            raise ValueError("Need Epochs to compute Confidence Interval")
         names = [evoked.comment for evoked in dataset]
     elif isinstance(dataset, Epochs) or isinstance(dataset, BaseEpochs):
         names = list(dataset.event_id.keys())
     else:
         raise ValueError("Input must be either a list of evoked responses or epoched data")
-    fig, ax = plt.subplots(len(groups),sharex="all", sharey="all")
+    fig, ax = plt.subplots(len(groups), sharex="all", sharey="all")
     fig.colorbar(cmap, ax=ax)
     if title is not None:
         fig.suptitle("%s mode: %s" % (title, mode))
     for i, group in enumerate(groups):
         for g in group:
-            idx = np.where(np.array(names)==g)[0][0]
-            if isinstance(dataset, list): # data is evoked
+            idx = np.where(np.array(names) == g)[0][0]
+            if isinstance(dataset, list):  # data is evoked
                 data = dataset[idx]
-            else: # data is epochs
+            else:  # data is epochs
                 data = dataset[names[idx]]
             data.crop(tmin, tmax)
             times = data.times
             data.apply_baseline(baseline)
-            if isinstance(dataset, list): # data is evoked
+            if isinstance(dataset, list):  # data is evoked
                 raw_data = data.data
             else:
                 raw_data = data._data
@@ -113,4 +117,3 @@ def compare_evokeds(dataset, groups, mode="gfp", title=None, subtitles=None, tmi
 
 def rational(x, a, b, c):
     return a/(x-c)+b
-
