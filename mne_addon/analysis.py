90a9d401b9809967a2e9d8d8dfb9313aa8dc4ed8
Ole <bialas.ole@gmx.de>
2020-03-19 08:31:42 +0100

    reformating

--- mne_addon/analysis.py
+++ mne_addon/analysis.py
@@ -1,5 +1,8 @@
-import numpy as np
-
+from mne.evoked import Evoked
+from mne.epochs import Epochs
+import numpy
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
 _scaling = 10**6  # scaing factor for the data
 
 
@@ -20,7 +23,7 @@ def noise_rms(epochs):
         if not i % 2:
             epochs_tmp._data[i, :, :] = -epochs_tmp._data[i, :, :]
     evoked = epochs_tmp.average().data
-    rms = np.sqrt(np.mean(evoked**2))*_scaling  # noise rms in micro volts
+    rms = numpy.sqrt(numpy.mean(evoked**2))*_scaling
     del epochs_tmp
     return rms
 
@@ -32,7 +35,7 @@ def signal_to_noise(epochs, signal_interval=(0.68, 0.72)):
     signal = epochs.copy()
     signal.crop(signal_interval[0], signal_interval[1])
     n_rms = noise_rms(epochs)
-    s_rms = np.sqrt(np.mean(signal.average().data**2))*_scaling
+    s_rms = numpy.sqrt(numpy.mean(signal.average().data**2))*_scaling
     snr = s_rms/n_rms  # signal rms divided by noise rms
     return snr
 
@@ -40,32 +43,96 @@ def signal_to_noise(epochs, signal_interval=(0.68, 0.72)):
 def global_field_power(data):
     """
     Compute the global field power which is the standard deviation of the
-    squared evoked response if input data is 3-dimensional it is considered
+    squared evoked response if inumpyut data is 3-dimensional it is considered
     as epoched data and averaged over the first dimension (number
     of epochs) to obtain the evoked response.
     """
+    if isinstance(data, Evoked) or isinstance(data, Epochs):
+        data = data.data
     if data.ndim == 3:
-        evoked = np.mean(data, axis=0)
+        evoked = numpy.mean(data, axis=0)
     elif data.ndim == 2:
         evoked = data
     else:
         raise ValueError("Data must be either 2-dimensional (evoked responses)"
                          "or 3-dimensional (epochs)")
-    return np.std(evoked**2, axis=0)
+    return numpy.std(evoked**2, axis=0)
 
 
 def global_rms(data):
     """
     Compute the square-root of the mean over all channels after squaring
-    (= root mean square).If input data is 3-dimensional it is considered as
+    (= root mean square).If inumpyut data is 3-dimensional it is considered as
     epoched data and averaged over the first dimension (numberof epochs)
     to obtain the evoked response.
     """
     if data.ndim == 3:
-        evoked = np.mean(data, axis=0)
+        evoked = numpy.mean(data, axis=0)
     elif data.ndim == 2:
         evoked = data
     else:
         raise ValueError("Data must be either 2-dimensional (evoked responses)"
                          " or 3-dimensional (epochs)")
-    return np.sqrt(np.mean(evoked**2, axis=0))
+    return numpy.sqrt(numpy.mean(evoked**2, axis=0))
+
+
+def find_peaks(data, min_dist=1, thresh=0.3, degree=None):
+    """
+    Find peaks in the data, optionally apply a baseline before.
+    min_dist: minimum distance between peaks, the biggest peak is preferred.
+    thresh: float between 0.0 and 1.0, normalized detection threshold
+    degree: degree of the polynomial that will estimate the data baseline.
+    if None no baseline detection is done
+    """
+    import peakutils
+    if degree is not None:
+        base = peakutils.baseline(data, degree)
+    else:
+        base = numpy.zeros(len(data))
+    peak_idx = peakutils.indexes(data-base, thres=thresh, min_dist=min_dist)
+    return peak_idx, base
+
+
+def peak_clustering(latency, amplitude, k=3, max_k=10, plot=True):
+    """
+    Use kmeans clustering to divide peak amplitudes of time series data into
+    temporal clusters. The clustering is done for different cluster numbers.
+    Each time, the squared sum of errors (SSE) is calculated to see which is
+    the optimal number of clusters for the dataset. Creates a plot with two
+    subplots if plot=True. Top: SSE versus number of clusters, bottom:
+    scatterplot of amplitudes vs latency, divided into clusters by color.
+    Parameters:
+        latency: array-like containing the peak latencies
+        amplitude: array-like containing the peak amplitudes
+        k: int, number of clusters for the clustered scatterplot
+        max_k: int, number of clusters to try for the SSE plot
+    Returns:
+        cluster_intervals:
+
+    """
+    data = numpy.array([latency, amplitude]).T
+    sse = {}
+    for this_k in range(1, max_k):
+        kmeans = KMeans(
+            n_clusters=this_k, max_iter=1000).fit(data)
+        # Inertia: Sum of distances of samples to their closest cluster center
+        sse[this_k] = kmeans.inertia_
+    kmeans = KMeans(n_clusters=k, max_iter=1000).fit(data)
+    clusters = kmeans.predict(data)
+    centers = kmeans.cluster_centers_
+    if plot:
+        fig, ax = plt.subplots(2)
+        fig.suptitle("Peak Amplitude Clustering")
+        ax[0].plot(list(sse.keys()), list(sse.values()), c="black")
+        ax[0].set_xlabel("Number of cluster")
+        ax[0].set_ylabel("SSE")
+        ax[1].scatter(latency, amplitude, c=clusters)
+        ax[1].scatter(centers[:, 0], centers[:, 1], c='black', s=100)
+        ax[1].set_xlabel("Peak Latency")
+        ax[1].set_ylabel("Peak Amplitude")
+        plt.show()
+    cluster_intervals = []
+    for i in range(k):
+        idx = numpy.where(clusters == i)[0]
+        cluster_intervals.append((min(latency[idx]), max(latency[idx])))
+    return cluster_intervals
--- mne_addon/plotting.py
+++ mne_addon/plotting.py
@@ -1,16 +1,18 @@
 from matplotlib import pyplot as plt
 from mne.viz import plot_evoked_topo
 from scipy.optimize import curve_fit
-import numpy as np
+import numpy as numpy
 import matplotlib.cm as cmx
 import matplotlib.colors as colors
 from mne.stats import bootstrap_confidence_interval
 from mne_addon.analysis import global_rms, global_field_power
 from mne.epochs import Epochs, BaseEpochs
 from mne.evoked import Evoked
+from sklearn.cluster import KMeans
 
 
-def plot_multiple_ERP(epochs, n_row=3, n_col=2, outfile=None, title=None, plot_topo=True):
+def plot_multiple_ERP(epochs, n_row=3, n_col=2, outfile=None, title=None,
+                      plot_topo=True):
     """
     Plot ERP for the different conditions
     """
@@ -22,7 +24,8 @@ def plot_multiple_ERP(epochs, n_row=3, n_col=2, outfile=None, title=None, plot_t
         raise ValueError("Size of the plot and number of experimental"
                          "conditions dont match!")
     for col in range(n_col):
-        for row, condition in zip(range(n_row), conditions[col*n_row:col*n_row+n_row]):
+        for row, condition in zip(range(n_row),
+                                  conditions[col*n_row:col*n_row+n_row]):
             epochs[condition].average().plot(axes=ax[row, col])
             ax[row, col].set_title("evoked response "+condition)
     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
@@ -41,13 +44,15 @@ def plot_fit(x, y, function, p0=None, xtitle=None, ytitle=None):
     """
     plt.scatter(x, y, color="k", marker=".", alpha=0.3)
     par, _ = curve_fit(function, x, y, p0=p0)
-    par = np.round(par, 2)
+    par = numpy.round(par, 2)
     label = "a/(x-c)+b with a=%s, b=%s, c=%s" % (par[0], par[1], par[2])
-    plt.plot(np.unique(x), function(np.unique(x), *par), color="k", label=label)
+    plt.plot(numpy.unique(x), function(numpy.unique(x), *par), color="k",
+             label=label)
     plt.legend()
 
 
-def plot_cluster_analysis(epochs_1, epochs_2, test_stat, clusters, p_values, title):
+def plot_cluster_analysis(epochs_1, epochs_2, test_stat, clusters, p_values,
+                          title):
 
     times = epochs_1.times
     info = epochs_1.info
@@ -55,22 +60,26 @@ def plot_cluster_analysis(epochs_1, epochs_2, test_stat, clusters, p_values, tit
         if p_values[i_c] <= 0.05:  # found significant cluster
             fig, ax = plt.subplots(2)
             fig.suptitle(title)
-            ax[0].plot(times, np.abs(epochs_1.average().data.mean(
+            ax[0].plot(times, numpy.abs(epochs_1.average().data.mean(
                 axis=0) - epochs_2.average().data.mean(axis=0)))
-            channels = np.unique(np.where(c != 0)[0])
-            samples = np.unique(np.where(c != 0)[1])
+            channels = numpy.unique(numpy.where(c != 0)[0])
+            samples = numpy.unique(numpy.where(c != 0)[1])
             for ch in channels:
                 ax[1].plot(times, test_stat[ch], label=info["ch_names"][ch])
-            ax[1].axvspan(times[min(samples)], times[max(samples)], color='r', alpha=0.3)
+            ax[1].axvspan(times[min(samples)], times[max(samples)],
+                          color='r', alpha=0.3)
             plt.legend()
             print(i_c)
 
 
-def compare_evokeds(dataset, groups, mode="gfp", title=None, subtitles=None, tmin=None, tmax=None, vline=None, baseline=None, color_coding=None, ci=None):
+def compare_evokeds(dataset, groups, mode="gfp", title=None, subtitles=None,
+                    tmin=None, tmax=None, vline=None, baseline=None,
+                    color_coding=None, ci=None):
     """
     Compare different evoked responses
     """
-    cNorm = colors.Normalize(vmin=min(color_coding.values()), vmax=max(color_coding.values()))
+    cNorm = colors.Normalize(vmin=min(color_coding.values()),
+                             vmax=max(color_coding.values()))
     cmap = cmx.ScalarMappable(norm=cNorm, cmap="cool")
     if mode == "rms":
         stat_fun = global_rms
@@ -78,21 +87,23 @@ def compare_evokeds(dataset, groups, mode="gfp", title=None, subtitles=None, tmi
         stat_fun = global_field_power
     else:
         raise ValueError("Mode must be either 'rms' or 'gfp'!")
-    if isinstance(dataset, list) and all(isinstance(d, Evoked) for d in dataset):
+    if isinstance(dataset, list) and all(
+            isinstance(d, Evoked) for d in dataset):
         if ci is not None:
             raise ValueError("Need Epochs to compute Confidence Interval")
         names = [evoked.comment for evoked in dataset]
     elif isinstance(dataset, Epochs) or isinstance(dataset, BaseEpochs):
         names = list(dataset.event_id.keys())
     else:
-        raise ValueError("Input must be either a list of evoked responses or epoched data")
+        raise ValueError(
+            "Input must be either a list of evoked responses or epoched data")
     fig, ax = plt.subplots(len(groups), sharex="all", sharey="all")
     fig.colorbar(cmap, ax=ax)
     if title is not None:
         fig.suptitle("%s mode: %s" % (title, mode))
     for i, group in enumerate(groups):
         for g in group:
-            idx = np.where(np.array(names) == g)[0][0]
+            idx = numpy.where(numpy.array(names) == g)[0][0]
             if isinstance(dataset, list):  # data is evoked
                 data = dataset[idx]
             else:  # data is epochs
@@ -105,14 +116,54 @@ def compare_evokeds(dataset, groups, mode="gfp", title=None, subtitles=None, tmi
             else:
                 raw_data = data._data
             data = stat_fun(raw_data)
-            ax[i].plot(times, data, color=cmap.to_rgba(color_coding[names[idx]]), label=g)
+            ax[i].plot(times, data, color=cmap.to_rgba(
+                color_coding[names[idx]]), label=g)
             if ci is not None:
-                ci_low, ci_high = bootstrap_confidence_interval(raw_data, stat_fun=stat_fun)
+                ci_low, ci_high = \
+                    bootstrap_confidence_interval(raw_data, stat_fun=stat_fun)
                 ax[i].fill_between(times, ci_low, ci_high)
             if subtitles is not None:
                 ax[i].set_title(subtitles[i])
             if vline is not None:
                 ax[i].axvline(vline, linestyle="--", c="black")
+
+
+def peak_clustering(latency, amplitude, k=3, max_k=10):
+    """
+    Use kmeans clustering to divide peak amplitudes of time series data into
+    temporal clusters. The clustering is done for different cluster numbers.
+    Each time, the squared sum of errors (SSE) is calculated to see which is
+    the optimal number of clusters for the dataset. Creates a plot with two
+    subplots. Top: SSE versus number of clusters, bottom: scatterplot of
+    amplitudes vs latency, divided into clusters by color.
+    Parameters:
+        latency: array-like containing the peak latencies
+        amplitude: array-like containing the peak amplitudes
+        k: int, number of clusters for the clustered scatterplot
+        max_k: int, number of clusters to try for the SSE plot
+
+    """
+    data = numpy.array([latency, amplitude]).T
+    fig, ax = plt.subplots(2)
+    fig.suptitle("Peak Amplitude Clustering")
+    sse = {}
+    for this_k in range(1, max_k):
+        kmeans = KMeans(
+            n_clusters=this_k, max_iter=1000).fit(data)
+        # Inertia: Sum of distances of samples to their closest cluster center
+        sse[this_k] = kmeans.inertia_
+    ax[0].plot(list(sse.keys()), list(sse.values()), c="black")
+    ax[0].set_xlabel("Number of cluster")
+    ax[0].set_ylabel("SSE")
+
+    kmeans = KMeans(n_clusters=k, max_iter=1000).fit(data)
+    cluster = kmeans.predict(data)
+    centers = kmeans.cluster_centers_
+    ax[1].scatter(latency, amplitude, c=cluster)
+    ax[1].scatter(centers[:, 0], centers[:, 1], c='black', s=100)
+    ax[1].set_xlabel("Peak Latency")
+    ax[1].set_ylabel("Peak Amplitude")
+    plt.show()
 
 
 def rational(x, a, b, c):
