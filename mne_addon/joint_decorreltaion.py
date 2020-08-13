fe27c87a4c413653e0c01eb82e1f319d2e094384
Ole <bialas.ole@gmx.de>
2020-08-13 13:39:12 +0200

    add reprojection

--- mne_addon/joint_decorreltaion.py
+++ mne_addon/joint_decorreltaion.py
@@ -2,7 +2,7 @@ import mne
 from matplotlib import pyplot as plt
 import numpy as np
 
-"""
+
 # Use MNE data
 data_path = mne.datasets.sample.data_path()
 raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
@@ -15,9 +15,11 @@ tmin, tmax = -0.2, 0.5
 reject = dict(eeg=80e-6)
 epochs = mne.Epochs(raw, events, tmin=-0.2, tmax=0.5, picks=('eeg'),
                     baseline=None, reject=reject, preload=True)
-epochs = epochs[["1", "2"]]  # only use auditory events
-"""
+con1, con2 = "1", "2"  # the conditions to compare
+epochs = epochs[[con1, con2]]  # only use auditory events
 
+
+"""
 con1, con2 = "b-mb", "b-mt"  # the conditions to compare
 # use own data
 epochs = mne.read_epochs("/home/ole/projects/elevation/data/eegl03/eegl03_mid-epo.fif")
@@ -25,6 +27,9 @@ epochs = epochs[[con1, con2]]
 epochs.filter(None, 30)
 tmin, tmax = 0.5, 1.1
 epochs.crop(tmin, tmax)
+"""
+
+2
 # epochs.interpolate_bads()
 # subtract mean per epoch
 epochs._data -= np.expand_dims(epochs._data.mean(axis=2), axis=2)
@@ -39,29 +44,30 @@ X.shape
 
 keep1 = 40  # number of components to keep after first rotation
 keep2 = 15  # number of components to keep after second rotations
+keep3 = 3
 
 C0 = X.T @ X  # Data covariance Matrix
-D, P = np.linalg.eig(C0)  # eigendecomposition of C0
+D, P1 = np.linalg.eig(C0)  # eigendecomposition of C0
 idx = np.argsort(D)[::-1][0:keep1]  # sort array by descending magnitude
 D = D[idx]
-P = P[:, idx]
+P1 = P1[:, idx]
 
 # check the result of the first PCA by plotting the components:
-comp1 = X @ P
+comp1 = X @ P1
 comp1 = np.reshape(comp1.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
 evoked1 = mne.EvokedArray(np.mean(comp1, axis=0), mne.create_info(
     keep1, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
 evoked1.plot()
 
-N = np.diag(np.sqrt(1. / D))  # diagonal whitening matrix
+N1 = np.diag(np.sqrt(1. / D))  # diagonal whitening matrix
 
 fig, ax = plt.subplots(1, 3)
 ax[0].imshow(C0)
-ax[1].imshow(P)
-ax[2].imshow(N)
+ax[1].imshow(P1)
+ax[2].imshow(N1)
 plt.show()
 
-Z = X @ P @ N  # sphered signal
+Z = X @ P1 @ N1  # sphered signal
 
 # epoch and plot the sphered data:
 comp2 = np.reshape(Z.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
@@ -72,53 +78,51 @@ evoked2.plot()
 L = np.tile(np.identity(epochs._data.shape[2]), epochs._data.shape[0])  # bias filter
 Zbar = L @ Z  # filter data
 C1 = Zbar.T @ Zbar  # Covariance of filtered data
-Dz, Q = np.linalg.eig(C1)  # eigendecomposition of bias filtered covariance matrix
-idx = np.argsort(Dz)[::-1]
+Dz, Q1 = np.linalg.eig(C1)  # eigendecomposition of bias filtered covariance matrix
+idx = np.argsort(Dz)[::-1][0:keep2]
 Dz = Dz[idx]
-Q = Q[:, idx]
+Q1 = Q1[:, idx]
 
 # plot distribution of eigenvalues for both decompositions:
 plt.semilogy(D, marker="o", label="first eigendecomposition")
 plt.semilogy(Dz, marker="o", label="second eigendecomposition")
 plt.show()
 
-Ybar = Zbar @ Q  # Components from the bias filtered data
-W = P @ N @ Q  # The unmixing matrix
-Y = X @ W  # Components from the unfiltered data
+Ybar = Zbar @ Q1  # Components from the bias filtered data
+W1 = P1 @ N1 @ Q1  # The unmixing matrix
+Y = X @ W1  # Components from the unfiltered data
 
 # Y and Ybar is the same except for the scaling --> Ybar needs to be devided by
 # n_epochs:
 
 evoked3 = mne.EvokedArray(Ybar.T, mne.create_info(
-    keep1, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
+    keep2, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
 evoked3.plot()
 
 comp4 = np.reshape(Y.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
 evoked4 = mne.EvokedArray(np.mean(comp4, axis=0), mne.create_info(
-    keep1, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
+    keep2, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
 evoked4.plot()
 
 # This Y is the activity that is maximally reproducible across trials. Now we want the activty
 # that is distinct between conditions
 
-X = Y[:, 0:keep2]  # only keep some components
-C2 = Y.T @ Y
-
-C0 = X.T @ X  # Data covariance Matrix
-D, P = np.linalg.eig(C0)  # eigendecomposition of C0
+C0 = Y.T @ Y  # Data covariance Matrix
+D, P2 = np.linalg.eig(C0)  # eigendecomposition of C0
 idx = np.argsort(D)[::-1][0:keep1]  # sort array by descending magnitude
 D = D[idx]
-P = P[:, idx]
-N = np.diag(np.sqrt(1. / D))  # diagonal whitening matrix
+P2 = P2[:, idx]
+N2 = np.diag(np.sqrt(1. / D))  # diagonal whitening matrix
+plt.imshow(P2)
 
 
-comp1 = X @ P
+comp1 = Y @ P2
 comp1 = np.reshape(comp1.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
 evoked1 = mne.EvokedArray(np.mean(comp1, axis=0), mne.create_info(
     keep2, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
 evoked1.plot()
 
-Z = X @ P @ N  # sphered signal
+Z = Y @ P2 @ N2  # sphered signal
 
 # epoch and plot the sphered data:
 comp2 = np.reshape(Z.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
@@ -133,27 +137,39 @@ L = np.concatenate([
 
 Zbar = L @ Z  # filter data
 C1 = Zbar.T @ Zbar  # Covariance of filtered data
-Dz, Q = np.linalg.eig(C1)  # eigendecomposition of bias filtered covariance matrix
-idx = np.argsort(Dz)[::-1]
+Dz, Q2 = np.linalg.eig(C1)  # eigendecomposition of bias filtered covariance matrix
+idx = np.argsort(Dz)[::-1][0:keep3]
 Dz = Dz[idx]
-Q = Q[:, idx]
+Q2 = Q2[:, idx]
 
 plt.semilogy(D, marker="o", label="first eigendecomposition")
 plt.semilogy(Dz, marker="o", label="second eigendecomposition")
 plt.show()
 
-Ybar = Zbar @ Q  # Components from the bias filtered data
-W = P @ N @ Q  # The unmixing matrix
-Y = X @ W  # Components from the unfiltered data
+Ybar = Zbar @ Q2  # Components from the bias filtered data
+W2 = P2 @ N2 @ Q2  # The unmixing matrix
+Y2 = Y @ W2  # Components from the unfiltered data
 
 # Y and Ybar is the same except for the scaling --> Ybar needs to be devided by
 # n_epochs:
 
 evoked3 = mne.EvokedArray(Ybar.T, mne.create_info(
-    keep2, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
+    keep3, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
 evoked3.plot()
 
-comp4 = np.reshape(Y.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
+comp4 = np.reshape(Y2.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
 evoked4 = mne.EvokedArray(np.mean(comp4, axis=0), mne.create_info(
-    keep2, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
+    keep3, epochs.info["sfreq"], ch_types="eeg"), tmin=tmin)
 evoked4.plot()
+
+# now go back into the sensor space:
+N1_inv = np.diag(1/np.diag(N1))
+N2_inv = np.diag(1/np.diag(N2))
+
+X_re = Y2 @ Q2.T @ N2_inv @ P2.T @ Q1.T @ N1_inv @ P1.T
+epochs_re = epochs.copy()
+epochs_re._data = np.reshape(X_re.T, [-1, n_epochs, n_times]).transpose([1, 0, 2])
+
+np.reshape(X_re.T, [-1, n_epochs, n_times]).transpose([1, 0, 2]).shape
+
+X_re.shape
