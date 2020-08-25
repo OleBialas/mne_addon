42f4dabc2cfe4a57689b8b978ffe75f9a36a18cd
OleBialas <bialas.ole@gmx.de>
2020-08-25 14:55:51 +0200

    add bootstrap method

--- mne_addon/joint_decorreltaion.py
+++ mne_addon/joint_decorreltaion.py
@@ -1,16 +1,26 @@
 import mne
 import numpy as np
+from scipy.signal import detrend
 
 
 class JointDecorrelation:
 
+    """
+    Implementation of Joint Decorrelation from de Cheveigné & Parra [2014] to
+    1) Find components (linear combinations of sensors that maximize either evoked
+    activity or the difference between two experimental conditions and
+    2) Reproject a subset of components onto the sensor space to filter the data
+    """
+
     def __init__(self, kind="evoked"):
+        if kind not in ["evoked", "difference"]:
+            raise ValueError("kind must be either 'evoked' or 'difference'!")
         self.kind = kind
         self.mixing = None
         self.unmixing = None
 
     def fit(self, epochs, keep1=None, keep2=None, keep3=None,
-            condition1=None, condition2=None):
+            condition1=None, condition2=None, detrending=True):
 
         if keep1 is None:
             keep1 = epochs.info["nchan"]
@@ -21,19 +31,20 @@ class JointDecorrelation:
         if condition1 is None and condition2 is None and self.kind == "difference":
             if len(list(epochs.event_id.keys())) != 2:
                 raise ValueError("If conditions are not specified, "
-                                 "epochs must contain exctly two types of events!")
+                                 "epochs must contain exactly two types of events!")
             else:
                 condition1, condition2 = list(epochs.event_id.keys())
-
-        epochs._data -= np.expand_dims(epochs._data.mean(axis=2), axis=2)  # might be a bad idea
         n_epochs, n_channels, n_times = epochs._data.shape
         if self.kind == "evoked":
             X = epochs._data
         elif self.kind == "difference":  # sort by condition
             X = np.concatenate([epochs[condition1]._data, epochs[condition1]._data])
+        if detrending:
+            X = detrend(X, axis=-1, type="linear")
+            print("detrending data...")
         X = np.transpose(epochs._data, (1, 0, 2))
         X = X.reshape(n_channels, n_epochs * n_times).T
-        L = L = np.tile(np.identity(n_times), n_epochs)
+        L = np.tile(np.identity(n_times), n_epochs)
         P, N, Q = JointDecorrelation._transform(X, L, keep1, keep2)
         self.unmixing = P @ N @ Q
         self.mixing = Q.T @ np.diag(1/np.diag(N)) @ P.T
@@ -93,7 +104,27 @@ class JointDecorrelation:
         return P, N, Q
 
 
+def bootstrap_components(epochs, n_bootstrap=1000, keep1=40, keep2=15, ci=0.95):
+    # pre allocate data matrix for bootstrap runs
+    # TODO: implement for kind=="difference"
+    n_epochs, n_channels, n_times = epochs._data.shape
+    data = np.zeros([n_bootstrap, keep2, n_times])
+    data.shape
+    for i in range(n_bootstrap):
+        indices = np.random.choice(np.arange(n_epochs, dtype=int), n_epochs, replace=True)
+        permutated_epochs = epochs.copy()
+        permutated_epochs._data = permutated_epochs._data[indices, :, :]
+        jd = JointDecorrelation(kind="evoked")
+        jd.fit(permutated_epochs, keep1=40, keep2=15)
+        Y = jd.get_components(permutated_epochs)
+        data[i, :, :] = Y.average().data
+        ci = (((1 - ci) / 2) * 100, ((1 - ((1 - ci) / 2))) * 100)
+        ci_low, ci_up = np.percentile(np.abs(data), ci, axis=0)
+    return ci_low, ci_up
+
 if __name__ == "__main":
+    
+    from matplotlib import pyplot as plt
     data_path = mne.datasets.sample.data_path()
     raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
     event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
@@ -103,11 +134,15 @@ if __name__ == "__main":
     reject = dict(eeg=80e-6)
     epochs = mne.Epochs(raw, events, tmin=-0.2, tmax=0.5, picks=('eeg'),
                         baseline=None, reject=reject, preload=True)
+    epochs._data = detrend(epochs._data, axis=-1)
     con1, con2 = "1", "2"  # the conditions to compare
     epochs = epochs[[con1, con2]]  # only use auditory events
     jd = JointDecorrelation(kind="difference")
     jd.fit(epochs, keep1=40, keep2=5)
-    y = jd.get_components(epochs)
-    y.average().plot()
-    x = jd.reproject_components(epochs)
-    x.average().plot()
+    Y = jd.get_components(epochs).average().data
+    ci_low, ci_up = bootstrap_components(epochs, n_bootstrap=1000, keep1=40, keep2=15, ci=0.95)
+    Y.shape
+
+    i_component = 0
+    plt.plot(epochs.times, np.abs(Y[i_component, :]))
+    plt.fill_between(epochs.times, ci_low[i_component, :], ci_up[i_component, :])
